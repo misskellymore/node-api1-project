a63d10c80f08ef1153c6ec0f52a1135f3ffe1b88
Kelly Moreira <moreira.kd@outlook.com>
2019-10-01 18:37:04 -0400

    some fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // instead of import use require
 const express = require('express');
+const db = require('./data/db.js');
 
 const server = express();
 
@@ -13,41 +14,49 @@ const users = require('./data/db.js');
 
 
 server.use(express.json());
+// middleware
 
 // Post
 
 
-server.post('/users', (req, res) => {
-    const userInfo = req.body;
-
-    if(userInfo.hasOwnProperty('name') && userInfo.hasOwnProperty('bio')) {
-
-        Users.insert(userInfo)
-        .then(user => {
-            res.status(201).json(user);
-        })
-
-        .catch(err => {
-            res.status(500).json({err: 'Please provide name and bio for the user.'})
-        })
-    } else {
-
-        res.status(400).json({errorMessage: 'There was an error while saving the user to the database'});
-    }   
-
+server.post('/api/users', (req, res) => {
+    console.log(req.body)
+    const {name, bio} = req.body;
+    if (!name || !bio) {
+        res.status(400).json({err: 'need name and bio'});
+    }  else {
+        db.insert({ name, bio })
+          .then(({ id }) => {
+            db.findById(id)
+              .then(user => {
+                res.status(201).json(user);
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({error: "server error retrieving user"});
+              });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({error: "server error inserting user"});
+          });
+        }
+    });
     
-})
+        
 
 
 
 
 // Get
 
-server.get('/users', (req, res) => {
-    users.find()
-    .then(users => {
+server.get('/api/users', (req, res) => {
+    console.log(db.find());
+    db.find()
+    .then(users => 
         res.status(200).json(users)
-    })
+        // 200 means good
+    )
     .catch(err => {
         res.status(500).json({error: 'The users information could not be retrieved.'})
     })
@@ -59,17 +68,21 @@ server.get('/users', (req, res) => {
 // GET User ID
 
 
-server.get('/user/:id', (req, res) => {
-    const userId = req.params.id;
+server.get('/api/users/:id', (req, res) => {
+    const { userId } = req.params.id;
 
-    Users.findById(userId)
-    .then(user => {
+    db.findById(userId)
+    .then(user => 
+        
+        {
         if (user) {
             res.status(200).json(user);
         } else {
             res.status(404).json({message: 'The user with the specified ID does not exist.'});
         }
-    })
+    }
+    // 404 means not found
+    )
 
     .catch( err => {
         res.status(500).json({err: 'The user information could not be retrieved.'});
@@ -79,11 +92,10 @@ server.get('/user/:id', (req, res) => {
 
 // Delete
 
-server.delete('/users/:id', (req, res) => {
-    const userId = req.params.id;
-
-
-    User.remove(userId)
+server.delete('/api/users/:id', (req, res) => {
+    
+    db.remove(req.params.id)
+    
     .then(user => {
         if (user) {
             res.status(200).json(user);
@@ -101,19 +113,26 @@ server.delete('/users/:id', (req, res) => {
 
 // Put w/user ID
 
-server.put('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const changes = req.body;
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const {name, bio} = req.body;
 
-
-    Users.update(userId, changes)
+    if (!name && !bio) {
+        res.status(400).json({err: 'need changes'});
+    }
+    db.update(id, {name, bio})
     .then(updated => {
         if (updated) {
-            res.status(200).json(updated);
+            db.findById(id)
+            .then( user => res.status(200).json(user))
+            .catch(err => {
+                res.status(500).json({err: 'error getting user'});
+            });
         } else {
             res.status(404).json({errorMessage: 'The user with the specified ID does not exist.'});
         }
-    })
+
+    })    
     .catch(err => {
         res.status(500).json({err: 'The user information could not be modified.'})
     })
